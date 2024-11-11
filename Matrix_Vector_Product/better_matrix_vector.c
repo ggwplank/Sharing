a7505001db19e63401d9fbbd22eacc09686fdeab
simoneniro17 <simoneniro17@gmail.com>
2024-11-11 12:35:00 +0100

    refactoring

--- Matrix_Vector_Product/better_matrix_vector.c
+++ Matrix_Vector_Product/better_matrix_vector.c
@@ -12,28 +12,29 @@ float fdot(int n, float *v1, float *v2) {
 }
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
+    if (argc != 2) {
         printf("Usage: %s <dimensione_matrice>\n", argv[0]);
         return 1;
     }
 
-    int i, n = atoi(argv[1]); // Dimensione della matrice n x n passata come argomento
+    int i, n = atoi(argv[1]); // dimensione matrice NxN passata come argomento
 
-    // Inizializza MPI
     MPI_Init(&argc, &argv);
+
     int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    // Creazione del gruppo e comunicatore personalizzato
+    // creazione gruppo e comunicatore personalizzato
     MPI_Group world_group, custom_group;
     MPI_Comm custom_comm;
     MPI_Comm_group(MPI_COMM_WORLD, &world_group);
 
     int ranks[world_size];
-    for (i = 0; i < world_size; i++) {
+    for (i = 0; i < world_size; i++)
         ranks[i] = i;
-    }
+
+    // crazione gruppo con tutti i rank del comunicatore globale
     MPI_Group_incl(world_group, world_size, ranks, &custom_group);
     MPI_Comm_create(MPI_COMM_WORLD, custom_group, &custom_comm);
 
@@ -46,36 +47,36 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    // Allocazione dinamica della matrice m1 e del vettore v2
+    // allocazione dinamica matrice e vettore
     float **m1 = (float **)malloc(n * sizeof(float *));
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
         m1[i] = (float *)malloc(n * sizeof(float));
-    }
+    
     float *v2 = (float *)malloc(n * sizeof(float));
+
     float local_result = 0.0;
     float *result = (float *)malloc(n * sizeof(float));
 
-    // Generazione della matrice m1 e del vettore v2 con valori casuali
+    // processo radice genera casualmente m1 e v2
     if (custom_rank == 0) {
         srand(time(NULL));
         for (i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++)
                 m1[i][j] = (float)(rand() % 10);
-            }
+            
             v2[i] = (float)(rand() % 10);
         }
     }
 
-    MPI_Barrier(custom_comm);  // Prima barriera
-
-    // Inizio del calcolo del tempo
+    // sincronizza processi prima di iniziare il calcolo
+    MPI_Barrier(custom_comm);
     double start_time = MPI_Wtime();
 
-    // Calcola il numero di elementi locali per ogni processo
-    int local_n = n / custom_size + (custom_rank < n % custom_size ? 1 : 0);
-    float *local_row = (float *)malloc(local_n * sizeof(float));
-    float *local_v2 = (float *)malloc(local_n * sizeof(float));
+    int local_n = n / custom_size + (custom_rank < n % custom_size ? 1 : 0);    // numero di elementi locali per ogni processo
+    float *local_row = (float *)malloc(local_n * sizeof(float));    // riga locale
+    float *local_v2 = (float *)malloc(local_n * sizeof(float)); // vettore locale
 
+    // counts e displs per la Scatterv di v2
     int counts_v2[custom_size], displs_v2[custom_size];
     int offset = 0;
     for (int p = 0; p < custom_size; p++) {
@@ -86,6 +87,7 @@ int main(int argc, char** argv) {
 
     MPI_Scatterv(v2, counts_v2, displs_v2, MPI_FLOAT, local_v2, local_n, MPI_FLOAT, 0, custom_comm);
 
+    // counts e displs per la Scatterv di m1
     for (i = 0; i < n; i++) {
         int counts[custom_size], displs[custom_size];
         offset = 0;
@@ -99,24 +101,26 @@ int main(int argc, char** argv) {
 
         local_result = fdot(local_n, local_row, local_v2);
 
+        // raccoglie i risultati parziali e somma i risultati nel processo radice
         MPI_Reduce(&local_result, &result[i], 1, MPI_FLOAT, MPI_SUM, 0, custom_comm);
     }
 
-    MPI_Barrier(custom_comm);  // Seconda barriera
-
-    // Fine del calcolo del tempo
+    // sincronizza i processi dopo il calcolo prima di calcolare il tempo
+    MPI_Barrier(custom_comm);
     double end_time = MPI_Wtime();
     double elapsed_time = end_time - start_time;
 
-/*
-    
+    /*
+    // stampa risultato
+    if(custom_rank == 0) {
         printf("Product of matrix and vector is:\n");
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < n; i++)
             printf("%f\n", result[i]);
-        }
-        */
+    }
+    */
+
+    // scrittura su file
     if (custom_rank == 0) {
-        // Scrittura del risultato in un file CSV
         FILE *file = fopen("results.csv", "a");
         if (file) {
             fprintf(file, "%d, %f\n", n, elapsed_time);
@@ -126,15 +130,16 @@ int main(int argc, char** argv) {
         }
     }
 
-    for (i = 0; i < n; i++) {
+    // deallocazione memoria
+    for (i = 0; i < n; i++)
         free(m1[i]);
-    }
     free(m1);
     free(v2);
     free(local_row);
     free(local_v2);
     free(result);
 
+    // deallocazione gruppo e comunicatore personalizzato
     MPI_Group_free(&custom_group);
     MPI_Comm_free(&custom_comm);
 
